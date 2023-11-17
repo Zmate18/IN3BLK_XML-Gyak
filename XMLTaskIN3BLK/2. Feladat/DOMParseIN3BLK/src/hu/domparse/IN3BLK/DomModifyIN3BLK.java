fe3dfccd0b1718e47edd313d8e5ac4b7ec37aa88
Zmate18 <mate.zavarko18@gmail.com>
2023-11-17 18:53:26 +0100

    Add new files and update existing files

---
+++ XMLTaskIN3BLK/2. Feladat/DOMParseIN3BLK/src/hu/domparse/IN3BLK/DomModifyIN3BLK.java
@@ -0,0 +1,10 @@
+package hu.domparse.IN3BLK;
+
+public class DomModifyIN3BLK {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ XMLTaskIN3BLK/2. Feladat/DOMParseIN3BLK/src/hu/domparse/IN3BLK/DomQueryIN3BLK.java
@@ -0,0 +1,10 @@
+package hu.domparse.IN3BLK;
+
+public class DomQueryIN3BLK {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
